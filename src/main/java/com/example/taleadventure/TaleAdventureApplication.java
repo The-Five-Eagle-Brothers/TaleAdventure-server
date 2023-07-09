c089c8aec9951fad315047146aa767a336dbfcf6
dayne-w <sdhdream99@naver.com>
2023-07-09 20:19:31 +0900

    first commit

---
+++ src/main/java/com/example/taleadventure/TaleAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.example.taleadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaleAdventureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaleAdventureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taleadventure/TaleAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taleadventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaleAdventureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
