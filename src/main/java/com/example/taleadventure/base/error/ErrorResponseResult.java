bb8d90f15602b17cd54d0d4d45bda6b9ce4606bd
dayne-w <sdhdream99@naver.com>
2023-07-10 19:50:33 +0900

    Feat: Add ErrorResponseResult 에러가 발생했을 때 Response 결과를 정의한 enum 클래스 정의 Resolves: #1

---
+++ src/main/java/com/example/taleadventure/base/error/ErrorResponseResult.java
@@ -0,0 +1,34 @@
+package com.example.taleadventure.base.error;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorResponseResult {
+    // 400 BAD_REQUEST
+
+    // 401 UNAUTHORIZED
+
+    // 403 FORBIDDEN
+
+    // 404 NOT_FOUND
+
+    // 405 METHOD_NOT_ALLOWED
+
+    // 406 NOT_ACCEPTABLE
+
+    // 409 CONFLICT
+
+    // 415 UNSUPPORTED_MEDIA_TYPE
+
+    // 500 INTERNAL_SERVER
+
+    // 502 BAD_GATEWAY
+
+    // 503 SERVICE_UNAVAILABLE
+
+    private final ErrorStatusCode statusCode;
+    private final String message;
+}
