f5e908dff4ec690465fdf7453ad3d0c75a6905fb
dayne-w <sdhdream99@naver.com>
2023-07-23 00:54:44 +0900

    FEAT : Member Entity 생성 Member Entity 생성함 Resolves: #1

---
+++ src/main/java/com/example/taleadventure/domain/member/entity/Member.java
@@ -0,0 +1,39 @@
+package com.example.taleadventure.domain.member.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+}
