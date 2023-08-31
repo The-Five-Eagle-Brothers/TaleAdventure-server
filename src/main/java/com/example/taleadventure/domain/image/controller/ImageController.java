266867676b36f77586911d1d7d4a681ba17683c4
dayne-w <sdhdream99@naver.com>
2023-08-31 21:53:43 +0900

    Feat: image를 받아 S3에 업로드 하는 api 메서드 구현 image 요청을 받아 S3에 해당 이미지를 저장하는 로직 구현 Resolves: #1

---
+++ src/main/java/com/example/taleadventure/domain/image/controller/ImageController.java
@@ -0,0 +1,23 @@
+package com.example.taleadventure.domain.image.controller;
+
+import com.example.taleadventure.base.dto.ApiSuccessResponse;
+import com.example.taleadventure.base.success.SuccessResponseResult;
+import com.example.taleadventure.domain.image.service.S3Upload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+
+    private final S3Upload s3Upload;
+
+    @PostMapping("/upload")
+    public ApiSuccessResponse<String> uploadFile (@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        return ApiSuccessResponse.successResponse(SuccessResponseResult.SUCCESS_CREATED, s3Upload.upload(multipartFile));
+    }
+}
