668f731608b1bdcc0c80ae6f0c1b9ecfd1035304
dayne-w <sdhdream99@naver.com>
2023-09-03 21:33:04 +0900

    Feat: ChapterController에 saveChapter api 메서드 생성 챕터 데이터 저장하는 api 메서드 생성 Resolves: #1

---
+++ src/main/java/com/example/taleadventure/domain/chapter/controller/ChapterController.java
@@ -0,0 +1,29 @@
+package com.example.taleadventure.domain.chapter.controller;
+
+import com.example.taleadventure.base.config.login.JwtHeaderUtil;
+import com.example.taleadventure.base.dto.ApiSuccessResponse;
+import com.example.taleadventure.base.success.SuccessResponseResult;
+import com.example.taleadventure.domain.talebook.dto.TaleBookInfoDto;
+import com.example.taleadventure.domain.talebook.dto.TaleBookRequest;
+import com.example.taleadventure.domain.talebook.service.TaleBookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chapter")
+public class ChapterController {
+
+    private final ChapterService chapterService;
+
+    @PostMapping("/save")
+    public ApiSuccessResponse<ChapterInfoDto> saveTaleBook(HttpServletRequest request, ChapterRequest chapterRequest, MultipartFile multipartFile){
+        String token = JwtHeaderUtil.getAccessToken(request);
+        return ApiSuccessResponse.successResponse(SuccessResponseResult.SUCCESS_CREATED, chapterService.saveChapter(chapterRequest, multipartFile));
+    }
+}
