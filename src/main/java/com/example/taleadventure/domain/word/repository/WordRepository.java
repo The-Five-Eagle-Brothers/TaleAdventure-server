0eb4632f9fe8b724d7dd2aac7705a958ba0516b0
dayne-w <sdhdream99@naver.com>
2023-09-06 22:06:09 +0900

    Feat: WordRepository 인터페이스 findAllByChapterTitle 추상 메서드 구현 구현 완료 Resolves: #1

--- src/main/java/com/example/taleadventure/domain/word/repository/WordRepository.java
+++ src/main/java/com/example/taleadventure/domain/word/repository/WordRepository.java
@@ -1,7 +1,15 @@
 package com.example.taleadventure.domain.word.repository;
 
+import com.example.taleadventure.domain.chapter.entity.Chapter;
 import com.example.taleadventure.domain.word.entity.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WordRepository extends JpaRepository<Word, Long> {
+
+    @Query("select w from Word w where w.chapter.title = :chapterTitle")
+    List<Word> findAllByChapterTitle(@Param("chapterTitle") String title);
 }
